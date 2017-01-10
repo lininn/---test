58ab0b74d18d314a52eedfe66981a99d79b130eb
lininn <liniaa@163.com>
2017-01-10 15:11:19 +0800

    1.10 15:10   初始化

---
+++ pages/weishi/weishi.js
@@ -0,0 +1,27 @@
+Page({
+  data:{
+    text:"my personal page",
+    showName:[
+        {"name":"笔记本"},
+        {"name":"记事本"},
+        {"name":"电视"},
+        {"name":"洗衣机"},
+      ]
+
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
